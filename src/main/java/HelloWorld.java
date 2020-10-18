0580f2aeb9fd7f9cfd813c208e0b39ee68d7a389
Raj-Release <55309718+Raj-Release@users.noreply.github.com>
2020-10-18 22:24:57 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,13 @@
-package helloworld;
+package hello;
+
+import org.joda.time.LocalTime;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("The current local time is: " + currentTime);
 
-	public static void main(String args[]){
-
-		System.out.println("Hello World, Maven");
-
-	}
-
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
 }
